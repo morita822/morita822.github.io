72c634d200a47975edc4ee71893586d41d924203
morita822 <tamo2.taro@gmail.com>
2023-04-13 10:47:26 +0900

    attendanceエラー修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,14 +3,14 @@ class AttendancesController < ApplicationController
   before_action :set_attendance, only: [:edit, :update, :approve]
 
   def index
-    @attendances = @employee.attendances.on_this_month.order(:worked_on)
+    @attendances = @employee.attendances.on_this_month.order(:date)
   end
 
   def show
   end
 
   def new
-    @attendance = @employee.attendances.build(worked_on: Date.current)
+    @attendance = @employee.attendances.build(date: Date.current)
   end
 
   def create
@@ -44,11 +44,11 @@ class AttendancesController < ApplicationController
     @employee = Employee.find_by(params[:employee_id])
   end
 
-  def set_attendance
+  def set_attendanc
     @attendance = Attendance.find(params[:id])
   end
 
   def attendance_params
-    params.require(:attendance).permit(:status, :worked_on, :note)
+    params.require(:attendance).permit(:date, :shift_status, :note)
   end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,12 +1,12 @@
 class Attendance < ApplicationRecord
-    belongs_to :employee
+  belongs_to :employee
+
+  enum attendance_status: { check_in: 1, check_out: 2 }
+
+  validates :date, presence: true
+  validates :attendance_status, presence: true
   
-    enum status: { check_in: 1, check_out: 2 }
-  
-    validates :status, presence: true
-    validates :worked_on, presence: true
-  
-    scope :on_this_month, -> { where(worked_on: Time.current.all_month) }
-    scope :approved, -> { where.not(approved_at: nil) }
-  end
-  
+
+  scope :on_this_month, -> { where(date: Time.current.all_month) }
+  scope :approved, -> { where.not(approved_at: nil) }
+end
